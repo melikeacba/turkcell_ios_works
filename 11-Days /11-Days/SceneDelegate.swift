56069cb3cbae45140850e2454e56863ccab5a89b
melikeacba <77928827+melikeacba@users.noreply.github.com>
2021-02-11 21:54:18 +0300

    9-10-11

---
+++ 11-Days /11-Days/SceneDelegate.swift
@@ -0,0 +1,67 @@
+//
+//  SceneDelegate.swift
+//  11-Days
+//
+//  Created by Melike Açba on 26.01.2021.
+//
+
+import UIKit
+import UserNotifications
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    let notifation = UNUserNotificationCenter.current()
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        UIApplication.shared.applicationIconBadgeNumber = 0
+        print("sceneDidBecomeActive call")
+        
+        notifation.requestAuthorization(options: [.alert, .badge, .sound]) { (status, error) in
+            if (status==true) {
+                print("status", status)
+                AppDelegate.notiStatus = true
+            }else{
+                print("Notication Permission False")
+                //print("status", (error?.localizedDescription)!) //turkce isl sis kullaniyosan turkce uyari alirsin
+            }
+        }
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+
+
+}
+
